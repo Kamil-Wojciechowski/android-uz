2889d0a42f13f4a7ada02d3c60b9a84925df636a
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2022-10-20 19:29:34 +0200

    Init

---
+++ app/src/main/java/com/example/foodcount/CustomerModel.java
@@ -0,0 +1,60 @@
+package com.example.foodcount;
+
+public class CustomerModel {
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    public CustomerModel(int id, String name, int age, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public CustomerModel() {
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
