9ca9aa51d820878e0e2b2e8afcc18cdb97e32910
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2022-10-23 10:48:13 +0200

    git commit -m "CU-2zc4htw - AA-003: Resources and validation"

--- app/src/main/java/com/example/foodcount/MainActivity.java
+++ app/src/main/java/com/example/foodcount/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.foodcount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,56 +17,87 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_add, btn_viewAll;
-    EditText et_name, et_age;
-    Switch sw_activeCustomer;
-    ListView lv_customerList;
+    EditText tv_firstname, tv_lastname, tx_email, tx_date,tx_phone;
+    Button bt_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_add = findViewById(R.id.bt_add);
-        btn_viewAll = findViewById(R.id.bt_viewAll);
-        et_age = findViewById(R.id.tx_age);
-        et_name = findViewById(R.id.tx_customerName);
-        sw_activeCustomer = findViewById(R.id.sw_customerActive);
-        lv_customerList = findViewById(R.id.lv_listView);
+        tv_firstname = findViewById(R.id.tx_firstname);
+        tv_lastname = findViewById(R.id.tx_lastname);
+        tx_date = findViewById(R.id.tx_date);
+        tx_email = findViewById(R.id.tx_email);
+        tx_phone = findViewById(R.id.tx_phone);
+        bt_save = findViewById(R.id.bt_save);
 
-        //btn listeners
-        btn_add.setOnClickListener(new View.OnClickListener() {
+
+
+        bt_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    CustomerModel customer = new CustomerModel(MainActivity.this, -1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
-                    Log.println(Log.INFO, "System", et_name.getText().toString());
-                    Boolean bool = customer.save();
-//                    DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
-//                    boolean bool = databaseHelper.addOne(customer);
-
-                    if(bool) {
-                        Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_LONG).show();
-                    }
-
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                if(!validateFirstname()) {
+                    return;
                 }
 
-
+                Toast.makeText(MainActivity.this, "Sucess!", Toast.LENGTH_LONG).show();
             }
         });
 
-        btn_viewAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                List<CustomerModel> everyone = new CustomerModel(MainActivity.this).getCustomers();
-
-                ArrayAdapter customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this, android.R.layout.simple_list_item_1, everyone);
-                lv_customerList.setAdapter(customerArrayAdapter);
-            }
-        });
     }
-}
+
+//        btn_add = findViewById(R.id.bt_add);
+//        btn_viewAll = findViewById(R.id.bt_viewAll);
+//        et_age = findViewById(R.id.tx_age);
+//        et_name = findViewById(R.id.tx_customerName);
+//        sw_activeCustomer = findViewById(R.id.sw_customerActive);
+//        lv_customerList = findViewById(R.id.lv_listView);
+
+        //btn listeners
+//        btn_add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                try {
+//                    CustomerModel customer = new CustomerModel(MainActivity.this, -1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+//                    Log.println(Log.INFO, "System", et_name.getText().toString());
+//                    Boolean bool = customer.save();
+//
+//                    if(bool) {
+//                        Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_LONG).show();
+//                    } else {
+//                        Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_LONG).show();
+//                    }
+//
+//                } catch (Exception e) {
+//                    Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_LONG).show();
+//                }
+//
+//
+//            }
+//        });
+//
+//        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                List<CustomerModel> everyone = new CustomerModel(MainActivity.this).getCustomers();
+//
+//                ArrayAdapter customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this, android.R.layout.simple_list_item_1, everyone);
+//                lv_customerList.setAdapter(customerArrayAdapter);
+//            }
+//        });
+//    }
+
+    private boolean validateFirstname() {
+        String val = tv_firstname.getText().toString().trim();
+
+        if(val.isEmpty()) {
+            tv_firstname.setError("Field can not be empty!");
+            return false;
+        }
+
+        tv_firstname.setError(null);
+        return true;
+        }
+
+    }
