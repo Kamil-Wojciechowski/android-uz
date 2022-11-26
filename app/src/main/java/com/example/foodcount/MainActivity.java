443752b2b17063982365a4df072ad423acb85196
Kamil Wojciechowski <wojciechowski.kamil1@outlook.com>
2022-11-26 11:51:26 +0100

    CU-2zc4j7v - AA-009: Notyfikacje

--- app/src/main/java/com/example/foodcount/MainActivity.java
+++ app/src/main/java/com/example/foodcount/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +20,51 @@ public class MainActivity extends AppCompatActivity {
         bt_userSettings=findViewById(R.id.bt_user_settings);
         bt_products = findViewById(R.id.bt_products);
 
-        bt_userSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, UserSettings.class));
-            }
-        });
+        bt_userSettings.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, UserSettings.class)));
 
-        bt_products.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Products.class));
-            }
-        });
+        bt_products.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Products.class)));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "Hello! We are starting application.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "Welcome back to the main activity", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "Resumed!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "You paused me!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "Bye bye!", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "I'm dying", Toast.LENGTH_LONG).show();
     }
 
     @Override
